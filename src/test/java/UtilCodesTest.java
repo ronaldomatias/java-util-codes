5717a0a3ea1aff45e04dedfa8ee240fc67345a20
ronaldomatias <ronaldomts476@gmail.com>
2022-10-12 20:25:22 -0300

    feat: convert first page from pdf to jpg image

---
+++ src/test/java/UtilCodesTest.java
@@ -0,0 +1,25 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class UtilCodesTest {
+
+    @DisplayName("Utilizando a lib 'Apache PDFBox', este método realiza a conversão da primeira página do documento, em uma imagem '.jpg' ")
+    @Test
+    public void convertFirstPageFromPDFToImage() throws IOException {
+
+        File file = new File("/home/ronaldo/Downloads/guia_rapido_merchandising_abr_2022_papo_de_farmacia.pdf");
+        PDDocument document = PDDocument.load(file);
+        BufferedImage image = new PDFRenderer(document).renderImageWithDPI(0, 600, ImageType.RGB);
+
+        ImageIO.write(image, "jpg", new File("/home/ronaldo/Desktop/image.jpg"));
+        document.close();
+    }
+}
